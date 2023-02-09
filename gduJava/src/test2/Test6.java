9ea68b3fb868ef4b6f3aced81d5f7ee913870d70
정진규 <rritjy@naver.com>
2023-02-09 17:32:53 +0900

    2023-02-09 17:32

--- gduJava/src/test2/Test6.java
+++ gduJava/src/test2/Test6.java
@@ -5,28 +5,30 @@ import java.util.Scanner;
 /*
 삼각형의 높이를 홀수로 입력하세요
 5
- *****
+*****
  ***
- *
+  *
  ***
- *****
+*****
  */
 public class Test6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("삼각형의 높이를 홀수로 입력하세요 : ");
 		int h = sc.nextInt();
+		int m = h/2;
+//		int i,j;
 
-		for(int i=1;i<=h;i++) {
+		for(int i=1;i<=m+1;i++) {
 			for(int j=1; j<i;j++) 
 				System.out.print(" ");
-			for(int j=i;j<=(h*2)-i;j++) 
+			for(int j=i;j<=((m+1)*2)-i;j++) 
 				System.out.print("*");
 			System.out.println();
 		}
 
-		for(int i=1;i<=h-1;i++) {
-			for(int j=i; j<=h-2;j++) {
+		for(int i=1;i<=m;i++) {
+			for(int j=i; j<=m-1;j++) {
 				System.out.print(" ");
 			}
 			for(int j=1; j<=(2*i)+1;j++) {
@@ -34,6 +36,8 @@ public class Test6 {
 			}
 			System.out.println();
 		}
+		
+			
+			
 	}
-
 }
